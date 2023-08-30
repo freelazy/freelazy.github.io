2e86f91d4cab34db6c9438fa9a12566064121bc3
freelazy234 <freelazy@some.day>
2023-08-30 14:23:04 +0800

    best_try_and_best_chance

--- demos/1.first-blood/hello.js
+++
@@ -1,6 +0,0 @@
-// 大小写敏感!!!
-function showTime () {
-    time_node.innerText = new Date();
-}
-
-time_button.onclick = showTime;
--- demos/4.java-base/Hello.java
+++
@@ -1,9 +0,0 @@
-public class Hello {
-    public static void main(String[] aaa) {
-          System.out.println("我要打印参数了\n");
-          for (int i = 0; i < aaa.length; i++) {
-               System.out.println(i + " : " + aaa[i]);
-          }
-          System.out.println("打印结束. 总共打印了 " + aaa.length + " 个参数");
-    }
-}
