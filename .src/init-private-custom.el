6bd99a0c820dba677ec24376c9a18cc9f11c5d65
freelazy <freelazy233@qq.com>
2023-11-02 16:38:10 +0800

    best_try_and_best_chance

--- .src/init-private-custom.el
+++ .src/init-private-custom.el
@@ -3,7 +3,7 @@
 
 (setq ic/host "imxx.top")
 (setq ic/proxy "http://imxx.top:1081" ic/proxy-lighter "   proxy   ")
-(setq ic/workdir "t:/dotnet/")
+(setq ic/workdir "d:/dotnet/")
 
 (setq find-program "d:/Apps/msys64/usr/bin/find.exe")
 
